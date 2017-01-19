e43266695f3bb184dcf01d0ffd3b6f14348bb9c6
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-19 09:22:41 -0500

    initial commit

---
+++ test_functions.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require_relative 'functions.rb'
+
+class TestMMFunctions < Minitest::Test
+	def test_one_equals_one()
+		assert_equal(1,1)
+	end
+	def test_pass_1_to_mined_minds_function_returns_1
+		assert_equal(1, mined_minds_function(1))
+	end
+end
